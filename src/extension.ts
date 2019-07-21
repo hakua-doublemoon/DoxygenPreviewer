003b45b6149bbd504ab5dd1b3bd742349bdba1ed
hakua-doublemoon <porteavionsdesmaidens@gmail.com>
2019-07-21 20:13:45 +0900

    update: [0.1.1] Enhanced Doxygen browsing function
    * Doxygenのドキュメントにおいて、リンクをそれなりに移動できるようにしました
    * 一時フォルダーの操作に失敗したときはエラーメッセージを出すようにして、拡張機能が落ちないようにしました
    * コマンドの実行に失敗したときはそのメッセージを出すようにしました

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,12 @@ import * as fsExtra from 'fs-extra';
 const env = process.env; // 環境変数からtmpフォルダを求める
 const output_dir = (env.Tmp ? path.join(env.Tmp, "DoxygenViewer") : "");
 var panel_alive = false;
+var self_ctrl_info = {
+    panel: {
+        current: "",
+        previous: "",
+    }
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -23,12 +29,20 @@ export function activate(context: vscode.ExtensionContext)
             //html_name = doxygen_exec(context.extensionPath);
         }
     );
-    let html_name = doxygen_exec(context.extensionPath);
+    let html_obj = doxygen_exec(context.extensionPath);
+
+    const insert_script_dir = path.join(context.extensionPath, "src", "insert_script");
+    const insert_script_uri = vscode.Uri.file(path.join(insert_script_dir, "message.js"));    
+    const insert_script_str = insert_script_uri.with({ scheme: 'vscode-resource' }).toString(true);
+    const css_path = vscode.Uri.file( path.join(output_dir, 'html', 'doxygen.css') );
+    const css_src  = css_path.with({ scheme: 'vscode-resource' }).toString(true);
+
     // 本当はhtml_nameが空文字だったらパネルを開きたくないが、ここで止めると拡張機能が有効に
     // ならないので止めない。影響としては真っ黒なパネルが開くぐらいっぽいので気にしなくてもいいことにする。
     let isFirst = true;
     const resource_root = [
         vscode.Uri.file(output_dir),
+        vscode.Uri.file(insert_script_dir),
     ];
     let panel_obj = vscode.window.createWebviewPanel(
         "doxy", 
@@ -45,6 +59,16 @@ export function activate(context: vscode.ExtensionContext)
         console.log("onDidDispose");
         panel_alive = false;
     });
+    panel_obj.webview.onDidReceiveMessage( (ev) => {
+        console.log("receive : " + ev);
+        if (!ev.data) {
+            return;
+        }
+        const data = ev.data;
+        if (ev.command === "link") {
+            next_html_load(data, panel_obj, css_src, insert_script_str);
+        }
+    });
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
@@ -56,17 +80,14 @@ export function activate(context: vscode.ExtensionContext)
         // Display a message box to the user
         //vscode.window.showInformationMessage('Hello World!');
         if (!isFirst) {
-            html_name = doxygen_exec(context.extensionPath);
+            html_obj = doxygen_exec(context.extensionPath);
         }
         isFirst = false;
-        console.log("html_name = " + html_name);
-        if (html_name == "") {
+        console.log("html_name = " + html_obj.load);
+        if (html_obj.load == "") {
             return;
         } 
 
-        // And get the special URI to use with the webview
-        const css_path = vscode.Uri.file( path.join(output_dir, 'html', 'doxygen.css') );
-        const cssSrc = css_path.with({ scheme: 'vscode-resource' }).toString(true);
         if (!panel_alive) {
             panel_obj = vscode.window.createWebviewPanel(
                 "doxy", 
@@ -86,26 +107,67 @@ export function activate(context: vscode.ExtensionContext)
         } else {
             panel_obj.webview.html = "";
         }
-        vscode.workspace.openTextDocument(html_name).then(doc=> {
-            console.log("set html doc");
-            panel_obj.webview.html = html_string_get(doc, cssSrc);
-        }); 
+
+        html_str_load(html_obj, panel_obj, css_src, insert_script_str);
     });
 
     context.subscriptions.push(disposable);
 }
 
-function html_string_get(doc: vscode.TextDocument, cssSrc : String) {
-    const css_link_tag = "<link href=\"" + cssSrc +"\" rel=\"stylesheet\" type=\"text/css\" />";
-    console.log("cssSrc = " + css_link_tag);
+function html_str_load(html_obj: {load: string, original: string}, 
+                       panel_obj: vscode.WebviewPanel, 
+                       css_src: string, 
+                       insert_script_str: string)
+{
+    if (!html_obj.load || !html_obj.original) {
+        return;
+    }
+    self_ctrl_info.panel.previous = self_ctrl_info.panel.current;
+    self_ctrl_info.panel.current = html_obj.original;
+    console.log("current : " + self_ctrl_info.panel.current);
+    vscode.workspace.openTextDocument(html_obj.original).then(doc=> {
+        console.log("set html doc");
+        panel_obj.webview.html = html_string_get(doc, css_src, insert_script_str);
+    });
+}
+
+function next_html_load(link_data: string, 
+                       panel_obj: vscode.WebviewPanel, 
+                       css_src: string, 
+                       insert_script_str: string)
+{
+    //const scrl_pos = link_data.replace(/^.+@/, "");
+    link_data = link_data.replace("file:\/\/\/", "").replace(/#.+/, "").replace(/@.+/, "");
+    const html_name = path.join(output_dir, "html", link_data);
+    console.log("next : " + html_name);
+    if (self_ctrl_info.panel.current == html_name) {
+        //const obj = {
+        //    command: "scroll",
+        //    data:    scrl_pos,
+        //}
+        //panel_obj.webview.postMessage(obj);
+        return;
+    }
+    const obj = {load: html_name, original: html_name};
+    html_str_load(obj, panel_obj, css_src, insert_script_str);
+}
+
+function html_string_get(doc: vscode.TextDocument, css_src: string, insert_script_str: string) {
     const buf = doc.getText().toString();
     let line_strs = buf.split(/\r\n|\n/);
+
+    const css_link_tag = "<link href=\"" + css_src +"\" rel=\"stylesheet\" type=\"text/css\" />";
+    //console.log("cssSrc = " + css_link_tag);
     line_strs.splice(15, 0, css_link_tag); // 15行目にcssの指定があるのでそれに合わせる。
+    const script_tag = `<script type="text/javascript" src="${insert_script_str}"></script>`;
+    line_strs.splice(13, 0, script_tag);
 
     let line_strs2 = line_strs.map( (fn) => {
         if (fn.includes("<title>")) {
             const title_rnd = Math.floor(Math.random() * 1000);
             return "<title>" + title_rnd + "</title>"
+        } else if (fn.includes("href=") && !fn.includes("css")) {
+            return fn.replace("href=\"", "href=\"file:\/\/\/"); // よくわからんがこれがないとリンクを移動しようとする
         } else {
             return fn;
         }
@@ -126,35 +188,49 @@ function src_name_of_workspace_get(path: String)
 
 function doxygen_exec(current_path: string)
 {
-    var ret_str = "";
-    const callback = () => { ret_str = doxygen_exec_main(current_path); };
+    var ret_obj = {load: "", original: ""};
+    const callback = () => {
+        ret_obj = doxygen_exec_main(current_path); 
+    };
     console.log("tmp = " + output_dir.toString());
     if (output_dir == "") {
-        return ret_str;
+        return ret_obj;
     }
-    if (fs.existsSync(output_dir)) {
-        //fs.rmdir(output_dir, () => { fs.mkdir(output_dir, callback); } );
-        fsExtra.removeSync(output_dir);
-        fs.mkdirSync(output_dir);
-        callback();
-    } else {
-        fs.mkdirSync(output_dir);
-        callback();
+    try {
+        if (fs.existsSync(output_dir)) {
+            //fs.rmdir(output_dir, () => { fs.mkdir(output_dir, callback); } );
+            fsExtra.removeSync(output_dir);
+            fs.mkdirSync(output_dir);
+            callback();
+        } else {
+            fs.mkdirSync(output_dir);
+            callback();
+        }
+    } catch(e) {
+        console.log("!! fs err : " + e);
+        const err_msg = e.toString();
+        if (err_msg.includes("Command failed")) {
+            vscode.window.showErrorMessage('Sorry something was happend. Your system is x86?');
+        } else {
+            vscode.window.showErrorMessage('Please close %Temp% directory');
+        }
     }
-    return ret_str;
+
+    return ret_obj;
 }
 
 function doxygen_exec_main(current_path: string)
 {
+    let ret_obj = {load: "", original: ""}
     let editor = vscode.window.activeTextEditor;
     if (!editor) { // これがないと警告される
-        return "";
+        return ret_obj;
     }
     let working_file_path = editor.document.fileName;
 
     const working_file_name = src_name_of_workspace_get(working_file_path);
     if (working_file_name == "") {
-        return "";
+        return ret_obj;
     }
     const dst_path = path.join(output_dir, working_file_name);
     console.log('target path = ' + dst_path);
@@ -167,24 +243,27 @@ function doxygen_exec_main(current_path: string)
     const file_list = fs.readdirSync(html_dir);
     const src_file_name = camel_to_snake(working_file_name.split(".")[0]);
     let html_name = "";
+    let original_name = "";
     console.log("src_file_name = " + src_file_name);
     if (!file_list) {
-        return "";
+        return ret_obj;
     }
     file_list.forEach(
         function(fn) {
             if (fn.includes(src_file_name) && !fn.includes("_source")) {
-                const original_html_name = path.join(html_dir, fn).toString();
+                original_name = path.join(html_dir, fn).toString();
                 const rndn = Math.floor(Math.random() * 1000);
                 const fn_rnd = rndn.toString() + ".html";
                 html_name = path.join(html_dir, fn_rnd).toString();
-                fs.copyFileSync(original_html_name, html_name); // ファイル名が同じだとロードしてくれないので
+                fs.copyFileSync(original_name, html_name); // ファイル名が同じだとロードしてくれないので
                 return;
             }
         }
     );
 
-    return html_name;
+    ret_obj.load     = html_name;
+    ret_obj.original = original_name;
+    return ret_obj;
 }
 
 // https://qiita.com/thelarch/items/cc4707e1c7ef0d73ba73
---
+++ src/insert_script/message.js
@@ -0,0 +1,70 @@
+const vscode = acquireVsCodeApi();
+function vscode_post_msg(command, data)
+{
+    obj = {
+        command: command,
+        data:    data,
+    }
+    vscode.postMessage(obj);
+}   
+
+var counter = 0;
+function count_test()
+{
+    counter = counter + 1;
+    //console.log(i);
+    //vscode_post_msg("test", i);
+}
+setInterval("count_test()",1000);
+
+function link_message(href_str)
+{
+    //console.log("link_message <= " + href_str);
+    const line_id = href_str.replace(/^.+#/, "");
+    if (line_id.length > 2) {
+        var elm = document.getElementById(line_id);
+        if (elm) {
+            elm.scrollIntoView();
+        }    
+    }
+    //console.log("link_message <= " + href_str);
+    //vscode_post_msg("link", href_str + "@" + scrl_pos);
+    vscode_post_msg("link", href_str);
+}
+
+window.onload = () => {
+    var a_tags = document.getElementsByTagName("a");
+    console.log("number of a = " + a_tags.length);
+    window.scrollTo(0,0);
+    for (var i = 0; i < a_tags.length; i+=1)
+    {   
+        var element = a_tags.item(i);
+        //console.log(element);
+        if (!element.className === "el") {
+            continue;
+        }
+        if (element.href.length < 5 || element.href.includes("http")) {
+            continue;
+        }
+        const href_str = element.href;
+        const position = element.getBoundingClientRect();
+        const scrl_pos = position.top;
+        //console.log(href_str);
+        element.addEventListener('click',
+            () => {
+                link_message(href_str);
+                //console.log(element);
+            }
+        )
+    }
+}
+
+window.addEventListener('message', event => {
+    console.log(event);
+    const message = event.data; // The JSON data our extension sent
+    switch (message.command) {
+      case 'scroll':
+        window.scrollTo(0,message.data);
+        break;
+    }
+});
